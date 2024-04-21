b740be48d35bb30bc1c0c3d8595ba71e909be31f
neilkapadia7 <neilkapadia7@gmail.com>
2024-04-21 15:17:01 +0530

    Middleware and Route Updates

---
+++ middleware/auth.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken');
+const Users = require("@models/User");
+
+module.exports = async function (req, res, next) {
+	const authorizationHeader = req.headers["authorization"]
+	if (!authorizationHeader) return res.status(401).json({
+		status: false,
+		message: "Access denied. No token provided."
+	});
+	const authorization = authorizationHeader.split(" ");
+	const token = authorization[1];
+
+	if (!token) {
+		return res.status(402).json({ message: 'No Token, Authorization Denied' });
+	}
+	try {
+		// const decoded = jwt.verify(token, config.get('jwtSecret'));
+		const decoded = jwt.verify(token, process.env.jwtSecret);
+
+		req.user = decoded.user.id;
+		req.userId = decoded.user.id;
+		
+		req.isAdminUser =false;
+		let checkUser = await Users.findById({_id: req.user}, {isAdminUser: 1});
+		if(checkUser) {
+			if(checkUser.isAdminUser)
+				req.isAdminUser = checkUser.isAdminUser
+		} else {
+			return res.status(401).json({ message: 'User not found' });
+		}
+
+		
+		next();
+	} catch (err) {
+		console.log(err)
+		return res.status(401).json({ message: 'Token is not Valid' });
+	}
+};
--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,8 @@ const express = require('express');
 const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
-const User = require('@models/Users/User');
+const User = require('@models/Users');
 const AuthController = require('@controllers/Auth');
-const AdminController = require('@controllers/AdminController');
 const auth = require('@middleware/auth');
 
 // @route   GET    api/auth
@@ -12,7 +11,7 @@ const auth = require('@middleware/auth');
 // @access  Private
 router.get('/', auth, async (req, res) => {
 	try {
-		const user = await User.findById(req.user).select('-password').populate('instituteId');
+		const user = await User.findById(req.user).select('-password');
 		res.status(200).json({data: user});
 	} catch (error) {
 		console.error(err.message);
@@ -44,19 +43,21 @@ router.post(
 // @access  Public
 router.post(
 	'/signup',
+    auth,
 	[
 		check('name', 'Please Include Name').isString(),
 		check('email', 'Please Include a Valid Email Id').isEmail(),
-		check('password', 'Please enter your password').not().isEmpty(),
-		check('isManualUserGeneration', 'Please enter isManualUserGeneration').isBoolean().optional(),
-		check('accessType', 'Please enter your accessType').isString(),
-		check('batchId', 'Please enter your batchId').isString().optional(),
+		check('password', 'Please enter your password').not().isEmpty()
 	],
 	async (req, res) => {
 		
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
+		}
+
+        if(!req.isAdminUser) {
+			return res.status(401).json({message: "Invalid Access"})
 		}
 
 		AuthController.createUser(req, res);
@@ -95,9 +96,9 @@ router.post(
 router.get('/getAllUsers',
 	auth,
 	async (req, res) => {
-		// if(!req.isAdminUser) {
-		// 	return res.status(401).json({message: "Invalid Access"})
-		// }
+		if(!req.isAdminUser) {
+			return res.status(401).json({message: "Invalid Access"})
+		}
 
 		AdminController.getAllUsers(req, res);
 	}
