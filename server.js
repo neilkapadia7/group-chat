e93570e0469a35f187437f2aacc657a0d6437edc
neilkapadia7 <neilkapadia7@gmail.com>
2024-04-21 15:04:21 +0530

    Backend Setup Done

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const connectDB = require('./config/db');
+var cors = require('cors')
+require("dotenv").config();
+require("module-alias/register");
+
+const app = express();
+
+connectDB();
+
+app.use(express.json({ extended: false }));
+app.use(cors())
+
+app.get('/', (req, res) => {
+	res.json({ message: 'Welcome to Group Chat' });
+});
+
+
+// app.use('/api', require('@routes/api'));
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`Server Started on Port Number: ${PORT}`));
